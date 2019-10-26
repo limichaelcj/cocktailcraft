f544ae14c9dae05d76b69ebba157fd3ec6a43616
Michael Li <mcli.893@gmail.com>
2019-10-25 18:45:59 -0700

    add user actions elements and placeholder lists

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,23 @@
 class PagesController < ApplicationController
 
   def home
+    @actions = [
+      {
+        name: 'Search',
+        icon: 'search',
+        text: 'Find a diverse variety of cocktails from classics to experimental concoctions.'
+      },
+      {
+        name: 'Create',
+        icon: 'lab',
+        text: 'Craft your own cocktail recipes to share with the world.'
+      },
+      {
+        name: 'What can I make?',
+        icon: 'puzzle piece',
+        text: "Tell us what's in your fridge and we'll put together a list of cocktails for you."
+      }
+    ]
     render 'pages/home'
   end
 
