bb052615e4a2e27bbb68c757c1a56befa6b615ba
Michael Li <mcli.893@gmail.com>
2019-11-07 09:57:37 -0800

    refactor data partials to helper methods; add cocktail cards list

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,2 +1,11 @@
 module CocktailsHelper
+
+  def cocktail_creator(instance)
+    instance.user.nil? ? 'Classic Collection' : instance.user.name
+  end
+
+  def cocktail_image_path(instance)
+    cl_image_path(instance.photo.nil? ? 'cocktail.jpg' : instance.photo)
+  end
+
 end
