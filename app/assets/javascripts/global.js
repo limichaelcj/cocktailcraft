8c697d8d9ac1882c896fc473534ade15eb09bc50
Michael Li <mcli.893@gmail.com>
2019-11-06 18:21:29 -0800

    fix notification auto-destroy bug

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
 
+  // destroy notices after timeout
+  const timeout = setTimeout(() => {
+    $('.notifications-box .app-notice').transition('fade')
+  }, 8000)
+
   // close notifications
   $('.message .close')
     .on('click', function() {
+      clearTimeout(timeout)
       $(this)
       .closest('.message')
       .transition('fade')
     })
   ;
 
-  // destroy notices after timeout
-  setTimeout(() => {
-    $('.notifications-box .app-notice').transition('fade')
-  }, 8000)
 
 })
