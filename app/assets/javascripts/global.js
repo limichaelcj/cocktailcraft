d5672132c039ec9e86b638ffbaa15e1b82cef839
Michael Li <mcli.893@gmail.com>
2019-11-06 20:33:04 -0800

    integrate image preview for form file uploads

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,5 +1,11 @@
-$(document).ready(function() {
+$(document).ready(ready)
 
+function ready(){
+  handleNotificationsBox();
+  imageUploadPreview();
+}
+
+function handleNotificationsBox(){
   // destroy notices after timeout
   const timeout = setTimeout(() => {
     $('.notifications-box .app-notice').transition('fade')
@@ -14,6 +20,26 @@ $(document).ready(function() {
       .transition('fade')
     })
   ;
+}
 
-
-})
+function imageUploadPreview(){
+  $('.image-upload-preview').on('change', function(){
+    if (this.files && this.files[0]){
+      const target = $(`#${this.id}-preview`)[0]
+      target.classList.remove('hidden')
+      target.innerHTML = `<div style="position:relative;height:100%;width:100%;"><div class="ui active loader"></div></div>`
+      const reader = new FileReader();
+      reader.readAsDataURL(this.files[0]);
+      reader.onload = function(e){
+        const img = document.createElement('img')
+        img.src = e.target.result
+        Object.assign(img.style, {
+          height: '100%',
+          width: '100%',
+        })
+        target.innerHTML = '';
+        target.append(img);
+      }
+    }
+  })
+}
