d14caea199615129110fe00807b018f849bf4405
Michael Li <mcli.893@gmail.com>
2019-11-08 18:00:36 -0800

    tweak cocktail/edit layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
     "style=background-image:url(#{url});"
   end
 
+  def stringify_dose_amount(dose)
+    unit =  dose.measurement.name + ( dose.amount == '1' || dose.amount == 'one' ? '' : dose.measurement.plural )
+    "#{dose.amount} #{unit}"
+  end
+
 end
