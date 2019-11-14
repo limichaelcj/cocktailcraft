617cec18807310a6d0beb63b08b5f6a7c38bdaf3
Michael Li <mcli.893@gmail.com>
2019-11-13 17:27:02 -0800

    add user show page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 
+  resources :users, only: [:index, :show]
+
   resources :cocktails do
     collection do
       get 'remix'
