1ac1654436dea06e00538928ad0fcd615b872e3e
Michael Li <mcli.893@gmail.com>
2019-11-19 14:05:23 -0800

    improve faker seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'nokogiri'
 require 'json'
 require 'open-uri'
 require 'faker'
-
-ingredients_url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-ingredients_data = JSON.parse(open(ingredients_url).read)
 
 puts "Seeding database..."
 
@@ -35,32 +33,70 @@ puts "Seeding measurements..."
 end
 
 puts "Seeding common ingredients..."
+print "- Requesting ingredients from thecocktaildb.com/api..."
+ingredients_url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_data = JSON.parse(open(ingredients_url).read)
 ingredients_data['drinks'].each do |item|
   Ingredient.create(name: item['strIngredient1'].downcase)
 end
+puts "OK"
 
 puts "Seeding users..."
 
-6.times do |i|
-  name = Faker::Name.middle_name
+unames = []
+while unames.count < 6 do
+  name = Faker::Games::ElderScrolls.name
+  if not unames.include? name
+    unames << name
+  end
+end
+
+unames.each do |name|
   username = Faker::Internet.username(specifier: name)
   email = Faker::Internet.email(name: name)
   pw = Faker::Internet.password
-  puts "#{i+1}) #{username} :: #{email} :: #{pw}"
+  puts "- #{username} :: #{email} :: #{pw}"
   User.create!(name: username, email: email, password: pw, password_confirmation: pw)
 end
 
-all_users = User.all
-
 puts "Seeding cocktails..."
 
+# get cocktail names from seventhsanctum
+# print "- Requesting names from seventhsanctum.com..."
+# doc = Nokogiri::HTML(open("https://www.seventhsanctum.com/generate.php?Genname=mixeddrink"))
+# cnames = doc.css('div.Generator.Results > div > div').map { |div| div.inner_html }
+# puts "OK"
+cnames = [
+  "Abundance Mix",
+  "Angelic White Imagining",
+  "Brown Abyss",
+  "Chop Float",
+  "Devilish Magical Moonrise",
+  "Elemental Angelic Soldier",
+  "Famous Low Greatness",
+  "Forest Paradise",
+  "High Western Cut",
+  "Holy Demonic Sailor",
+  "Insane Passionate Scarlet",
+  "Kiwi Lime Mix",
+  "Modest Orange Walnut",
+  "Raging Cocoa",
+  "Royal Squirt",
+  "Tea Peach Martini",
+  "Unholy King's Rain",
+  "Unholy Nasty Garnet",
+  "Valorous Sea",
+  "Wonderful Annihilation Shandy",
+]
+
 # seed random cocktails
-20.times do |i|
+puts "- Creating cocktails with doses..."
+cnames.each_with_index do |name, i|
   cocktail = Cocktail.create!(
-    name: Faker::Coffee.blend_name,
-    description: Faker::Books::Lovecraft.sentence,
+    name: name,
+    description: Faker::Food.description,
     instructions: Array.new(rand(3...6)) { Faker::Lorem.paragraphs.join(' ') }.join(' '),
-    user: i > 10 ? nil : User.find(rand(1...all_users.count)),
+    user: i > 10 ? nil : User.find(rand(1...User.all.count)),
     published: true
   )
 
@@ -68,9 +104,23 @@ puts "Seeding cocktails..."
     Dose.create!(
       cocktail: cocktail,
       ingredient: Ingredient.find(rand(1...Ingredient.all.count)),
-      amount: rand(1...5),
+      amount: rand(2...5),
       measurement: Measurement.find(rand(1...Measurement.all.count))
     )
+  end
+end
+
+# seed reviews
+puts "Seeding reviews..."
+Cocktail.all.each do |c|
+  User.all.shuffle[0, rand(1..4)].each do |user|
+    Review.create!({
+      cocktail: c,
+      user: user,
+      rating: rand(1..5),
+      title: Faker::Books::Lovecraft.word,
+      text: Faker::Restaurant.review
+    })
   end
 end
 
