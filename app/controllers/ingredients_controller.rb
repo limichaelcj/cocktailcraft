bbac91b6a750f78ebaf2b4bf3433381932778220
Michael Li <mcli.893@gmail.com>
2020-01-17 12:06:24 -0800

    remove ingredients page

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,7 +1,2 @@
 class IngredientsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
-
-  def index
-    @ingredients = Ingredient.all.sort_by { |ing| -ing.recipes.count }
-  end
 end
